55466506f8312bacba23a951f8fc3f2fb34ccf41
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-16 00:04:58 +0530

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -1,15 +1,15 @@
 class Translation(object):
 
-    START_TEXT = '**Hello,** {}!\n\n**Send me the YouTube link of the video you want to upload**' 
+    START_TEXT = '__**Hello,** {}!\n\n**Send me the YouTube link of the video you want to upload**__' 
 
     ABOUT_TXT = """
 ╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
+├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/Luffy0000007)
 ├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
 ├💮 **Language** : [Python 3.12.3](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
+├👥 **Support Group** : [LUFFY BOTS SUPPORT](https://t.me/Luffy0000007)
+├🥏 **Channel** : [LUFFY BOT CHANNEL](https://t.me/Luffy0000007)
 ├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+├🎓 **Developer** : [ㅤᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ⃝ᡃ. Anonymous ❂℡](https://t.me/Luffy0000007)
 ╰───────────────⍟
 """
