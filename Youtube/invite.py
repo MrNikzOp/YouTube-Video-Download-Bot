640fc7f7f4eaa3a233fee4fa35d26af691c48a66
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-16 13:32:13 +0530

    Update invite.py

--- Youtube/invite.py
+++ Youtube/invite.py
@@ -5,4 +5,4 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
        		[ [ InlineKeyboardButton("📡 Sʜᴀʀᴇ Yᴏᴜʀ Lɪɴᴋ" ,url=f"https://t.me/share/url?url=https://t.me/Lufffybro_bot?start={message.from_user.id}") ]   ])
-    await message.reply_photo(photo="./luffy.jpg",reply_markup=reply_markup,)
+    await message.reply_photo(photo="./775f18aed5f6c9f3e5d332e158c092dd.jpg",reply_markup=reply_markup,)
