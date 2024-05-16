76080a6770bffff024941097bf160619767accd0
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-17 00:13:12 +0530

    Update command.py

--- Youtube/command.py
+++ Youtube/command.py
@@ -69,4 +69,24 @@ Enjoy using the bot!**__
     """
     message.reply_text(help_text)
 
+@Client.on_message(filters.command(["test"]) & filters.private)
+async def start(bot, update):
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
+    await bot.send_message(
+        Config.LOG_CHANNEL,
+           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
+    )
+    
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
+
 ########################🎊 Lisa | NT BOTS 🎊######################################################
