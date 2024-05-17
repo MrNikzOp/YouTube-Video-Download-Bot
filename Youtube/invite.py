472b3e466280d5bcd5ec8cd6c1b6ebb76d425d57
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-17 10:44:17 +0530

    Update invite.py

--- Youtube/invite.py
+++ Youtube/invite.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
-from Youtube.config import Config
+from Luffy.config import Config
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+
+photo= "./775f18aed5f6c9f3e5d332e158c092dd.jpg"
 @Client.on_message(filters.private & filters.command(["invite"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
@@ -8,11 +10,10 @@ async def refer(client,message):
        		  [ 
        		    InlineKeyboardButton("📡 Sʜᴀʀᴇ Yᴏᴜʀ Lɪɴᴋ" ,url=f"https://t.me/share/url?url=https://t.me/Lufffybro_bot?start={message.from_user.id}") ],
        		     [
-       		                    InlineKeyboardButton('👩‍💻ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Luffy0000007'),
+       		                    InlineKeyboardButton('👩‍💻ᴅᴇᴠᴇʟᴏᴘᴇʀr', url='https://t.me/Luffy0000007'),
        		                    InlineKeyboardButton('👥 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+qveQSMp0Hl9mMzdh'),
-       		                ],
-                [
-                    InlineKeyboardButton('⛔️ ᴄʟᴏꜱᴇ', callback_data='cancel')
-                ]
+       		                ]
        		    ])
-    await message.reply_photo(photo="./775f18aed5f6c9f3e5d332e158c092dd.jpg",caption="__**INVITE YOUR FRIENDS**__",reply_markup=reply_markup,)
+    await message.reply_photo(photo=random.choice(PICS), caption=START_TEXT.format(user=message.from_user.mention, bot=client.mention),reply_markup=reply_markup,)
+    
+    # [ LuffyBot ] #
