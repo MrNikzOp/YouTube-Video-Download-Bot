dbefafce7912bce2c88a9a2889ad9434a7e673aa
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-17 11:03:21 +0530

    Update invite.py

--- Youtube/invite.py
+++ Youtube/invite.py
@@ -3,7 +3,7 @@ from Youtube.config import Config
 from Youtube.script import Translation
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
-photo= "./775f18aed5f6c9f3e5d332e158c092dd.jpg"
+# photo= "./775f18aed5f6c9f3e5d332e158c092dd.jpg"
 @Client.on_message(filters.private & filters.command(["invite"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
@@ -15,6 +15,6 @@ async def refer(client,message):
        		                    InlineKeyboardButton('👥 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+qveQSMp0Hl9mMzdh'),
        		                ]
        		    ])
-    await message.reply_photo(photo=photo), caption=START_TEXT.format(message.from_user.first_name),reply_markup=reply_markup,)
+    await message.reply_photo(photo="./775f18aed5f6c9f3e5d332e158c092dd.jpg"), caption=START_TEXT.format(message.from_user.first_name),reply_markup=reply_markup,)
     
     # [ LuffyBot ] #
