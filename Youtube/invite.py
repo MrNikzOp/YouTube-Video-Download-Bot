38edcbf082476a968f9004da2563f81c48c3f528
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-17 10:48:58 +0530

    Update invite.py

--- Youtube/invite.py
+++ Youtube/invite.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from Luffy.config import Config
+from Youtube.config import Config
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
 photo= "./775f18aed5f6c9f3e5d332e158c092dd.jpg"
