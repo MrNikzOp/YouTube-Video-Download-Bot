9fb28c9811009cb4398959a733fb0be9ad3ba337
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-18 01:16:34 +0530

    Update list.py

--- Youtube/list.py
+++ Youtube/list.py
@@ -2,7 +2,13 @@ import os, random
 
 from pyrogram import Client, filters
 from Youtube.config import Config
+from Youtube.forcesub import handle_force_subscribe
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
- @Client.on_message(filters.private & filters.command(["list"]))
+@Client.on_message(filters.private & filters.command("list"))
+async def list(client, message):
+    if Config.CHANNEL:
+      fsub = await handle_force_subscribe(client, message)
+      if fsub == 400:
+        return
 await message.reply_photo(photo=random.choice(PICS), caption=HELP_TEXT.format(user=message.from_user.mention, bot=client.mention), reply_markup=reply_markup,)
