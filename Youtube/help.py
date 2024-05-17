f62e1c50b8ca9c854acdf0c9014f7b56952b72d3
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-17 19:10:38 +0530

    Update help.py

--- Youtube/help.py
+++ Youtube/help.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
-    from Youtube.config import Config
-    from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from Youtube.config import Config
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
     @Client.on_message(filters.private & filters.command(["test"]))
     async def refer(client,message):
         reply_markup = InlineKeyboardMarkup(
