d536c78304c332b368d9b898ede165b5fd6dbbe8
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-18 01:11:44 +0530

    Update list.py

--- Youtube/list.py
+++ Youtube/list.py
@@ -1,1 +1,8 @@
+import os, random 
 
+from pyrogram import Client, filters
+from Youtube.config import Config
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+
+ @Client.on_message(filters.private & filters.command(["list"]))
+await message.reply_photo(photo=random.choice(PICS), caption=HELP_TEXT.format(user=message.from_user.mention, bot=client.mention), reply_markup=reply_markup,)
