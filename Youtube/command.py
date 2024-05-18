c992c89a6b8a50273dad3132e0743493035c6947
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-18 10:59:00 +0530

    Update command.py

--- Youtube/command.py
+++ Youtube/command.py
@@ -13,6 +13,27 @@ from Youtube.forcesub import handle_force_subscribe
 async def cancel(client, callback_query):
     await callback_query.message.delete()
 
+@Client.on_callback_query()
+async def button(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Translation.HELP_TEXT,
+            reply_markup=Translation.HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Translation.ABOUT_TEXT,
+            reply_markup=Translation.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+
 # About command handler
 @Client.on_message(filters.private & filters.command("about"))
 async def about(client, message):
@@ -51,7 +72,8 @@ async def start(client, message):
                 InlineKeyboardButton('👥 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+qveQSMp0Hl9mMzdh'),
             ],
             [
-                InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel')
+                InlineKeyboardButton('♻️ About', callback_data='about'),
+                InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
             ]
         ]
     ))
