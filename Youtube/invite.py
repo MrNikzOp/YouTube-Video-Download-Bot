9bb36974676347de54f64de47bd34b4a36fbb36e
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-19 10:37:36 +0530

    Update invite.py

--- Youtube/invite.py
+++ Youtube/invite.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from Youtube.config import Config
+from Youtube.script import Translation
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
 @Client.on_callback_query(filters.regex("cancel"))
