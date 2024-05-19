96a7d5cfd0cf5dbf61efbcae463669d549ef49e8
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-19 22:01:13 +0530

    Update invite.py

--- Youtube/invite.py
+++ Youtube/invite.py
@@ -31,7 +31,7 @@ async def button(bot, update):
         
     elif update.data == "help":
         await update.message.edit_text(
-            text=Translation.HELP_TXT,
+            text=Translation.HELP_TEXT,
           #  reply_markup=Translation.ABOUT_BUTTONS,
            # disable_web_page_preview=True
             reply_markup=InlineKeyboardMarkup(
