d4e0362bf774de475d2aae8ff77d521cef11a4fd
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-21 23:23:12 +0530

    Update command.py

--- Youtube/command.py
+++ Youtube/command.py
@@ -64,12 +64,12 @@ async def about(client, message):
       fsub = await handle_force_subscribe(client, message)
       if fsub == 400:
         return 
-    photo_url = "https://telegra.ph/file/97e325476ebe8dd8676ad.jpg"
+  #  photo_url = "https://telegra.ph/file/97e325476ebe8dd8676ad.jpg"
   #  await message.reply_photo(photo="", caption="Test")      
-    await message.reply_photo(photo=photo_url, caption="Here is the photo you requested.")
+   # await message.reply_photo(photo=photo_url, caption="Here is the photo you requested.")
         
        # chat_id=message.from_user.id,
-       # text=Translation.ABOUT_TXT,
+        text=Translation.ABOUT_TXT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
         [
