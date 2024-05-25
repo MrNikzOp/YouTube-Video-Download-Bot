7b73dd05af8d607e0f41f343fa11ba81f61e7d0c
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-25 22:20:13 +0530

    Update command.py

--- Youtube/command.py
+++ Youtube/command.py
@@ -56,20 +56,6 @@ async def button(bot, update):
         ]
     ))
 
-  elif update.data == "premium":
-        await update.message.edit_text(
-            text=Translation.PREMIUM_TEXT.format(update.from_user.mention),
-           # reply_markup=Translation.ABOUT_BUTTONS,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-        [
-            [
-            InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='home'),
-            InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
-            ]
-        ]
-    ))
-        
             
 # About command handler
 @Client.on_message(filters.private & filters.command("about"))
