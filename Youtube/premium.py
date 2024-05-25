24437d53bf7b70b290a67420bc05d1ae2cfeddd7
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-25 11:34:14 +0530

    Update premium.py

--- Youtube/premium.py
+++ Youtube/premium.py
@@ -1,1 +1,103 @@
 
+import os, asyncio
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
+from Youtube.config import Config
+from Youtube.script import Translation
+from Youtube.forcesub import handle_force_subscribe
+
+########################🎊 Luffy | RJ BOTS 🎊######################################################
+@Client.on_callback_query(filters.regex("cancel"))
+async def cancel(client, callback_query):
+    await callback_query.message.delete()
+
+@Client.on_callback_query(filters.regex("home"))
+async def button(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=Translation.HOME_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton('📍 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/https://t.me/+qveQSMp0Hl9mMzdh'),
+            ],
+            [
+                InlineKeyboardButton('👩‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Luffy0000007'),
+                InlineKeyboardButton('👥 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+qveQSMp0Hl9mMzdh'),
+            ],
+            [
+                InlineKeyboardButton('♻️ Aʙᴏᴜᴛ', callback_data='about'),
+                InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
+            ]
+        ]
+    ))
+        
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Translation.HELP_TXT,
+           # reply_markup=Translation.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Translation.ABOUT_TXT,
+           # reply_markup=Translation.ABOUT_BUTTONS,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+        [
+            [
+            InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='home'),
+            InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
+            ]
+        ]
+    ))
+        
+            
+# About command handler
+@Client.on_message(filters.private & filters.command("details"))
+async def about(client, message):
+    if Config.CHANNEL:
+      fsub = await handle_force_subscribe(client, message)
+      if fsub == 400:
+        return 
+        await message.reply_text(
+       # chat_id=message.from_user.id,
+        text=Translation.ABOUT_TXT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='home'),
+                InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
+            ]
+        ]
+    ))
+
+
+# Start command handler
+@Client.on_message(filters.private & filters.command("pst"))
+async def start(client, message):
+    if Config.CHANNEL:
+      fsub = await handle_force_subscribe(client, message)
+      if fsub == 400:
+        return
+    #user = message.from_user
+    await message.reply_text(
+        text=Translation.START_TEXT.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton('📍 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/https://t.me/+qveQSMp0Hl9mMzdh'),
+            ],
+            [
+                InlineKeyboardButton('👩‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Luffy0000007'),
+                InlineKeyboardButton('👥 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+qveQSMp0Hl9mMzdh'),
+            ],
+            [
+                InlineKeyboardButton('♻️ Aʙᴏᴜᴛ', callback_data='about'),
+                InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
+            ]
+        ]
+    ))
