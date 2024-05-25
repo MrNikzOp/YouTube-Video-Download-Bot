02b7029d3f1c78eed8faa769b6a16800e5ba5793
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-25 13:17:20 +0530

    Update premium.py

--- Youtube/premium.py
+++ Youtube/premium.py
@@ -20,8 +20,8 @@ async def start(client, message):
       if fsub == 400:
         return
     #user = message.from_user
-      await message.reply_sticker("CAACAgUAAxkBAAEBvlVk7YKnYxIHVnKW2PUwoibIR2ygGAACBAADwSQxMYnlHW4Ls8gQHgQ")
-        await asyncio.sleep(2)
+    await message.reply_sticker("CAACAgUAAxkBAAEBvlVk7YKnYxIHVnKW2PUwoibIR2ygGAACBAADwSQxMYnlHW4Ls8gQHgQ")
+ #   await asyncio.sleep(2)
     await message.reply_text(
         text=Translation.PREMIUM_TEXT.format(message.from_user.mention),
         disable_web_page_preview=True,
