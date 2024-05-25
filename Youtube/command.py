bb3377281a6899e29a7259683ce365bbeb9fce64
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-25 22:58:34 +0530

    Update command.py

--- Youtube/command.py
+++ Youtube/command.py
@@ -8,54 +8,7 @@ from Youtube.config import Config
 from Youtube.script import Translation
 from Youtube.forcesub import handle_force_subscribe
 
-PIC = "https://telegra.ph/file/97e325476ebe8dd8676ad.jpg",
-########################🎊 Lisa | NT BOTS 🎊######################################################
-@Client.on_callback_query(filters.regex("cancel"))
-async def cancel(client, callback_query):
-    await callback_query.message.delete()
-
-@Client.on_callback_query(filters.regex("home"))
-async def button(bot, update):
-    if update.data == "home":
-        await update.message.edit_text(
-            text=Translation.HOME_TEXT.format(update.from_user.mention),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton('📍 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/https://t.me/+qveQSMp0Hl9mMzdh'),
-            ],
-            [
-                InlineKeyboardButton('👩‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Luffy0000007'),
-                InlineKeyboardButton('👥 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+qveQSMp0Hl9mMzdh'),
-            ],
-            [
-                InlineKeyboardButton('♻️ Aʙᴏᴜᴛ', callback_data='about'),
-                InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
-            ]
-        ]
-    )) 
-        
-    elif update.data == "help":
-        await update.message.edit_text(
-            text=Translation.HELP_TXT,
-           # reply_markup=Translation.ABOUT_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit_text(
-            text=Translation.ABOUT_TXT,
-           # reply_markup=Translation.ABOUT_BUTTONS,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-        [
-            [
-            InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='home'),
-            InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
-            ]
-        ]
-    ))
-
+########################🎊 Luffy | NT BOTS 🎊######################################################
             
 # About command handler
 @Client.on_message(filters.private & filters.command("about"))
