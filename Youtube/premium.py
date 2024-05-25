2759f18d27fb406c372936f0f79b7079b99a0724
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-25 14:09:08 +0530

    Update premium.py

--- Youtube/premium.py
+++ Youtube/premium.py
@@ -20,7 +20,7 @@ async def start(client, message):
       if fsub == 400:
         return
     #user = message.from_user
-    await message.reply_sticker("CAACAgUAAxkBAAEBvlVk7YKnYxIHVnKW2PUwoibIR2ygGAACBAADwSQxMYnlHW4Ls8gQHgQ")
+   m = await message.reply_sticker("CAACAgUAAxkBAAEBvlVk7YKnYxIHVnKW2PUwoibIR2ygGAACBAADwSQxMYnlHW4Ls8gQHgQ")
     await asyncio.sleep(2)
     await message.reply_text(
         text=Translation.PREMIUM_TEXT.format(message.from_user.mention),
@@ -32,4 +32,4 @@ async def start(client, message):
             ]
         ]
     ))
-    return await delete()
+    return await m.delete()
