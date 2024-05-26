3136616ad9cdc391681c7f1bee74aeca3ec75aa6
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-26 23:14:33 +0530

    Update callbacks.py

--- Youtube/callbacks.py
+++ Youtube/callbacks.py
@@ -11,17 +11,23 @@ PIC = "https://telegra.ph/file/97e325476ebe8dd8676ad.jpg",
 async def cancel(client, callback_query):
     await callback_query.message.delete()
 
-@Client.on_callback_query(filters.regex("home"))
+@Client.on_callback_query()
 async def button(bot, update):
     if update.data == "home":
         await update.message.edit_text(
-            text=Translation.HOME_TEXT.format(update.from_user.mention),
+            text=Translation.START_TEXT.format(update.from_user.mention),
             reply_markup=Translation.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Translation.HELP_TEXT,
+            reply_markup=Translation.HELP_BUTTONS,
             disable_web_page_preview=True
         )
     elif update.data == "about":
         await update.message.edit_text(
-            text=Translation.ABOUT_TXT,
+            text=Translation.ABOUT_TEXT,
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
