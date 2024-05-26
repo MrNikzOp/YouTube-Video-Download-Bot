4541b10520fac3a41fcf30abb2badfd52e9977ab
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-26 21:47:15 +0530

    Update info.py

--- Youtube/info.py
+++ Youtube/info.py
@@ -33,11 +33,8 @@ async def media_info(bot, m):
                                         
     if not md:
         buttons = [[
-            InlineKeyboardButton("✨️ Support", url="https://t.me/BETA_SUPPORT"),
-            InlineKeyboardButton("📢 Updates", url="https://t.me/Beta_BoTZ"),
-        ],
-            InlineKeyboardButton("Cʟᴏꜱᴇ ✘", callback_data="cancel")       
-                  ]       
+            InlineKeyboardButton("✘ Cʟᴏꜱᴇ", callback_data="cancel")
+        ]]       
         mkn = await m.reply("Pʟᴇᴀꜱᴇ Wᴀɪᴛ....")
         if ff.photo:
            user_dp = await bot.download_media(message=ff.photo.big_file_id)
