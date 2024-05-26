06c063c2b4af1573dc88ed9cd184c150be17469f
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-26 22:59:13 +0530

    Update callbacks.py

--- Youtube/callbacks.py
+++ Youtube/callbacks.py
@@ -34,7 +34,7 @@ async def button(bot, update):
     )) 
     elif update.data == "help":
         await update.message.edit_text(
-            text=Translation.HELP_TXT,
+            text=Translation.ABOUT_TXT,
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
             
