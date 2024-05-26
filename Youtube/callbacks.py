24538a95fe1e2dcefbc32531bb32baac41bc07d9
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-26 22:09:17 +0530

    Update callbacks.py

--- Youtube/callbacks.py
+++ Youtube/callbacks.py
@@ -32,20 +32,11 @@ async def button(bot, update):
             ]
         ]
     )) 
-        
     elif update.data == "premium":
         await update.message.edit_text(
             text=Translation.PREMIUM_DETAILS.format(update.from_user.mention),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-        [
-            [
-            InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='home'),
-            InlineKeyboardButton('✘ Cʟᴏꜱᴇ', callback_data='cancel'),
-            ]
-        ]
-    ))
-        
+            disable_web_page_preview=True        
+            )
     elif update.data == "about":
         await update.message.edit_text(
             text=Translation.ABOUT_TXT,
